d4a029ac14a00decab85c04d2ede915c824d6afe
Berkant Dursun <berkant.dursun@me.com>
2020-11-15 13:35:31 +0100

    Initial Commit

---
+++ AppIconResizer/AppIconResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIconResizerApp.swift
+//  AppIconResizer
+//
+//  Created by Berkant Dursun on 15.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AppIconResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIconResizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppIconResizer
+//
+//  Created by Berkant Dursun on 15.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
